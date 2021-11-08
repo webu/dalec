2ced682800b4d44bbca947db676f279ef3dcdca7
Samuel WEBER <s.weber@webu.coop>
2021-11-08 15:48:35 +0000

    Allow channel_object to contains arbirtrary characters, then un/quote it in view Add templatetags to parse string date/datetime from retrieved external APIs

--- dalec/urls.py
+++ dalec/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(
         (
             "(?P<app>[a-z][a-z0-9_-]+)/(?P<content_type>[a-z][a-z0-9_-]+)"  # required part
-            "(?:/(?P<channel>[a-z][a-z0-9_-]+)(?:/(?P<channel_object>[^/]+)?))?"  # opt part
+            "(?:/(?P<channel>[a-zA-Z][a-zA-Z0-9_-]+)(?:/(?P<channel_object>[^/]+)?))?"  # opt part
         ),
         FetchContentView.as_view(),
         name="dalec_fetch_content",
