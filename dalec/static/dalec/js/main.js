9fa41cecb7f37bb31dc477f7ec47c76226463705
Adrien Hamraoui <a.hamraoui@webu.coop>
2023-09-21 17:06:23 +0200

    chore: run prettier on main.js

--- dalec/static/dalec/js/main.js
+++ dalec/static/dalec/js/main.js
@@ -1,47 +1,40 @@
+function dalec_fetch_content(container) {
+  const [orderedBy, url] = [container.dataset.orderedBy, container.dataset.url];
+  let channelObjects = container.dataset.channelObjects;
 
-
-function dalec_fetch_content(container){
-    const [ orderedBy, url ] = [
-        container.dataset.orderedBy,
-        container.dataset.url
-    ];
-    let channelObjects = container.dataset.channelObjects;
-
-    container.classList.add("dalec-loading");
-    container.classList.remove("dalec-loading-error");
-    if (channelObjects !== undefined){
-        channelObjects = JSON.parse(channelObjects)
+  container.classList.add("dalec-loading");
+  container.classList.remove("dalec-loading-error");
+  if (channelObjects !== undefined) {
+    channelObjects = JSON.parse(channelObjects);
+  }
+  fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "text/html",
+      "Content-Type": "application/json",
+    },
+    // cache: "no-cache",
+    body: JSON.stringify({
+      channelObjects: channelObjects,
+      orderedBy: orderedBy,
+    }),
+    keepalive: true,
+  }).then(function (response) {
+    if (!response.ok) {
+      container.classList.remove("dalec-loading");
+      container.classList.add("dalec-loading-error");
+      console.error(`HTTP error ${response.status} while fetching ${url}`);
+      return;
     }
-    fetch(
-        url,
-        {
-            method: "POST",
-            headers: {
-              'Accept': 'text/html',
-              'Content-Type': 'application/json'
-            },
-            // cache: "no-cache",
-            body: JSON.stringify(
-                {"channelObjects": channelObjects, "orderedBy": orderedBy}
-            ),
-            keepalive: true,
-        }
-    ).then(function(response){
-        if (!response.ok) {
-            container.classList.remove("dalec-loading");
-            container.classList.add("dalec-loading-error");
-            console.error(`HTTP error ${response.status} while fetching ${url}`);
-            return ;
-        }
-        if (response.status === 204) {
-            container.classList.remove("dalec-loading");
-            return ;
-        }
-        response.text().then(function (html) {
-            container.innerHTML = html ;
-            container.classList.remove("dalec-loading");
-        });
+    if (response.status === 204) {
+      container.classList.remove("dalec-loading");
+      return;
+    }
+    response.text().then(function (html) {
+      container.innerHTML = html;
+      container.classList.remove("dalec-loading");
     });
+  });
 }
 
 module.exports = dalec_fetch_content;
