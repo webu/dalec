60402717a4b6754d11d56a03a2757844490d5b8b
Adrien Hamraoui <a.hamraoui@webu.coop>
2023-11-03 12:26:39 +0100

    chore(qa): add step to check no migration is missing

--- runqa.sh
+++ runqa.sh
@@ -10,3 +10,10 @@ flake8
 mypy -p dalec -p dalec_prime -p dalec_example
 coverage report -m
 coverage erase
+
+export DJANGO_SETTINGS_MODULE="tests.settings"
+if ! django-admin makemigrations --check; then
+    >&2 echo "Des migrations sont manquantes et doivent être générées"
+    django-admin makemigrations --dry-run
+    exit 1
+fi
